266f723d76ae45ae6bd4be3b1d356a2d02332191
jniemine <jniemine@c1r3p5.hive.fi>
2022-10-24 12:05:02 +0300

    normed tilde.c and unsetenv.c

--- src/tilde.c
+++ src/tilde.c
@@ -6,15 +6,15 @@
 /*   By: jakken <jakken@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 17:26:41 by jakken            #+#    #+#             */
-/*   Updated: 2022/10/22 18:38:49 by jakken           ###   ########.fr       */
+/*   Updated: 2022/10/24 12:03:39 by jakken           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-static void old_pwd(char *tilde, char **dest, char **environ_cp)
+static void	old_pwd(char *tilde, char **dest, char **environ_cp)
 {
-	char *temp;
+	char	*temp;
 
 	temp = NULL;
 	temp = search_variable(environ_cp, "OLDPWD");
@@ -30,9 +30,9 @@ static void old_pwd(char *tilde, char **dest, char **environ_cp)
 	ft_memdel((void **)&temp);
 }
 
-static void pwd(char *tilde, char **dest, char **environ_cp)
+static void	pwd(char *tilde, char **dest, char **environ_cp)
 {
-	char *temp;
+	char	*temp;
 
 	temp = NULL;
 	temp = search_variable(environ_cp, "PWD");
@@ -48,13 +48,13 @@ static void pwd(char *tilde, char **dest, char **environ_cp)
 	ft_memdel((void **)&temp);
 }
 
-int expand_tilde(char *tilde, char **dest, char **environ_cp)
+int	expand_tilde(char *tilde, char **dest, char **environ_cp)
 {
 	char	*temp;
 
 	if (tilde[1] == '/' || tilde[1] == '\0')
 	{
-		temp = search_variable(environ_cp, "HOME"); 
+		temp = search_variable(environ_cp, "HOME");
 		if (!temp)
 			ft_printf("minishell: $HOME unset\n");
 		else
@@ -67,13 +67,12 @@ int expand_tilde(char *tilde, char **dest, char **environ_cp)
 		ft_memdel((void **)&temp);
 	}
 	else if (tilde[1] == '-' && (tilde[2] == '\0'
-		|| tilde[2] == '/'))
+			|| tilde[2] == '/'))
 		old_pwd(tilde, dest, environ_cp);
 	else if (tilde[1] == '+' && (tilde[2] == '/'
-		|| tilde[2] == '\0'))
+			|| tilde[2] == '\0'))
 		pwd(tilde, dest, environ_cp);
 	else
 		*dest = ft_strdup(tilde);
-	//Whats the return val?
 	return (ft_strlen(tilde));
 }
--- src/unsetenv.c
+++ src/unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: jakken <jakken@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 16:39:24 by jakken            #+#    #+#             */
-/*   Updated: 2022/10/21 18:24:58 by jakken           ###   ########.fr       */
+/*   Updated: 2022/10/24 12:04:30 by jakken           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ms_unsetenv(char **args, char ***environ_cp)
 {
-	int 	i;
+	int	i;
 
 	i = 1;
 	while (args[i])
