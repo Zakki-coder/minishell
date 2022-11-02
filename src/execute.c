3546c5f33e003d7d8e248ae76bc0c8859bf7375e
Jakke Nieminen <jniemine@c1r4p11.hive.fi>
2022-11-02 11:46:08 +0200

    Fixed execute_bin, if there is / in given command, then it is handled as user executable else search from /Users/jniemine/.brew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki:/Users/jniemine/.brew/bin

--- src/execute.c
+++ src/execute.c
@@ -6,33 +6,11 @@
 /*   By: jniemine <jniemine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 17:45:18 by jakken            #+#    #+#             */
-/*   Updated: 2022/10/29 21:08:44 by jniemine         ###   ########.fr       */
+/*   Updated: 2022/11/02 11:36:58 by jniemine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-
-/*
-static char	**add_cwd_to_path(char **environ_cp)
-{
-	char	*path;
-	char	*new;
-	char	**bk;
-
-	bk = ft_cpynstrarr(environ_cp, calc_chptr(environ_cp));
-	path = search_variable(environ_cp, "PATH");
-	if (!path)
-	{
-		update_env("PATH", ".", &bk);
-		return (bk);
-	}
-	new = ft_strjoin(path, ":.");
-	ft_memdel((void **)&path);
-	update_env("PATH", new, &bk);
-	ft_memdel((void **)&new);
-	return (bk);
-}
-*/
 
 static void	exe_fail(char **cmd, char **args, char ***env_cp)
 {
@@ -41,65 +19,31 @@ static void	exe_fail(char **cmd, char **args, char ***env_cp)
 	ms_exit(args, env_cp);
 }
 
-static int count_char(char *str, char c)
+int	check_if_user_exe(char *cmd, char **dest)
 {
-	int	i;
-
-	i = 0;
-	while(*str)
+	*dest = NULL;
+	if (ft_strchr(cmd, '/'))
 	{
-		if (*str == c)
-			++i;
-		++str;
+		*dest = ft_strdup(cmd);
+		return (1);
 	}
-	return (i);
+	return (0);
 }
-
-/*
-static char	*search_cmd_from_cwd(char *cmd, char **environ_cp)
-{
-	char	*cwd;
-	char	**bk;
-	char	*res;
-
-	res = NULL;
-	if (cmd[0] == '.' && cmd[1] == '/')
-	{
-		bk = ft_cpynstrarr(environ_cp, calc_chptr(environ_cp));
-		cwd_wrap(&cwd);
-		update_env("PATH", cwd, &bk);
-		res = search_bin(cmd, bk);
-		ft_freeda((void ***)&bk, calc_chptr(bk));
-	}
-	return (res);
-}
-*/
 
 int	execute_bin(char **args, char ***environ_cp)
 {
 	char	*cmd;
 	int		id;
 	int		wstatus;
-//	char	**environ_bk;
 
-	if (!*args)
+	if (!args[0])
 		return (0);
-//	environ_bk = add_cwd_to_path(*environ_cp);
-	cmd = NULL;
-	if (args[0])
+	if (!check_if_user_exe(args[0], &cmd))
+		cmd = search_bin(args[0], *environ_cp);
+	if (!cmd || !ft_strchr(cmd, '/'))
+		ft_printf("minishell: %s: command not found...\n", args[0]);
+	else
 	{
-//		update_env("_", cmd, environ_cp);
-		if (args[0][0] && (args[0][0] == '/'
-			|| (args[0][0] == '.' && args[0][1] == '/')))
-			cmd = ft_strdup(args[0]);
-		else
-		{
-			// TODO Do something sensible when checking that filename must start with ./
-			cmd = search_bin(args[0], *environ_cp);
-			if (cmd && (count_char(cmd, '/') == 1 && cmd[0] != '/') && (count_char(cmd, '/') == 0
-			|| (count_char(cmd, '/') == 1 && (cmd[0] != '.' || cmd[1] != '/'))))
-			ft_memdel((void **)&cmd);
-		}
 		id = fork();
 		if (id == 0)
 		{
@@ -111,9 +55,6 @@ int	execute_bin(char **args, char ***environ_cp)
 		else
 			wait(&wstatus);
 	}
-	else
-		ft_printf("minishell: %s: command not found...\n", args[0]);
-//	ft_freeda((void ***)&environ_bk, calc_chptr(environ_bk));
-//	ft_memdel((void **)&cmd);
+	ft_memdel((void **)&cmd);
 	return (0);
 }
